a55937772da42366bbac6e6743bfbe429d5d37d7
Hoffit <dehull_1@yahoo.com>
2018-07-09 17:43:42 -0700

    Assignment Completed

---
+++ AbbreviateTwoWordName/solution.js
@@ -0,0 +1,19 @@
+function abbrevName(name) {
+
+
+  let spaceIndex = name.indexOf(' ');
+
+
+  let firstInitial = name.substring(0, 1);
+
+
+  let secondInitial = name.substring(spaceIndex + 1, spaceIndex + 2);
+
+
+  let rawInitials = `${firstInitial}.${secondInitial}`;
+
+
+  return rawInitials.toUpperCase();
+
+
+}
