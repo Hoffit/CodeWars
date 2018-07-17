5e40d6ee47b3c9f72d5d1b1060b9fd8950ae2cd1
Hoffit <dehull_1@yahoo.com>
2018-07-17 14:10:30 -0700

    Refined the kata solution a little.

--- Array-isNice/solution.js
+++ Array-isNice/solution.js
@@ -1,14 +1,13 @@
 function isNice(arr) {
 
-  if(!arr || arr.length === 0) return false;
+  if(!arr || !arr.length) return false;
 
-  let isNice = true;
   for(let i=0; i<arr.length && isNice; i++) {
 
     if(!(arr.includes(arr[i]-1) ||
       arr.includes(arr[i]+1))) {
-        isNice = false;
+        return false;
       }
   }
-  return isNice;
+  return true
 }
