8cb760e32b3a8d9114c5443622711443bb14efc3
Hoffit <dehull_1@yahoo.com>
2018-07-25 07:56:21 -0700

    made a fix should work now

--- highest-scoring-word/solution.js
+++ highest-scoring-word/solution.js
@@ -3,15 +3,15 @@
     let highIndex = 0;
     let highValue = 0;
     let currentValue = 0;
-    words.forEach((aWord) => {
-      currentValue = aWord.split('').reduce((accumulator, currentValue) => accumulator + currentValue.charCodeAt(0),0);
+    words.forEach((aWord, index) => {
+      console.log(aWord);
+      currentValue =
+        aWord.split('').reduce((accumulator, currentValue) => accumulator + currentValue.charCodeAt(0)%96,0);
+      console.log(currentValue);
       if(currentValue > highValue) {
         highIndex = index;
         highValue = currentValue;
       }
     });
-    console.log(words[highIndex]);
+    return words[highIndex];
   };
-  console.log(high('man i need a taxi up to ubud'));
-  console.log(high('what time are we climbing up the volcano'));
-  console.log(high('take me to semynak'));
